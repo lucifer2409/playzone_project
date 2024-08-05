735b883cc45a4f854756b18c185c25b7f6c2602b
lucifer2409 <trivedi.a@somaiya.edu>
2024-08-05 17:16:08 +0530

    This is the latest commit at 5:15pm

---
+++ playzone_next_app/app/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/global.css'
+
+
+const Navbar = () => {
+  return (
+    <div id='navbar'>
+        <h1 id='textInsideDiv'>PLAYZONE</h1>
+    </div>
+  )
+}
+
+export default Navbar
