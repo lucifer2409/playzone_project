7ce462a5e10b45fb1bb142ca033492f20a9cc9a7
lucifer2409 <trivedi.a@somaiya.edu>
2024-08-09 12:35:17 +0530

    This is the latest changes

--- app/main.tsx
+++ app/main.tsx
@@ -16,7 +16,7 @@ const Main = () => {
       </div>
       <div id='textSection'>
       <TypewriterEffectSmooth words={words} 
-        className="responsiveness-for-the-text-animation text-lg" 
+        className="text-4xl flex justify-center items-center w-full h-full" 
         cursorClassName="bg-blue-400"/>
         <span id='textInsideTextSection'>
         PlayZone simplifies the process of finding and booking sports turfs near you. Whether you’re looking for a quick match or planning a regular session, our app offers a seamless experience, letting you reserve your spot with just a few taps. Skip the hassle of phone calls and availability checks—PlayZone brings everything you need to your fingertips. Download our app now to start booking turfs effortlessly and enjoy a streamlined process that saves you time and energy!
